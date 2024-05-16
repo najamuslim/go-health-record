de495853cc3b6f56342134f51581348fa3600962
shani <mshaniasadulhaq@gmail.com>
2024-05-16 23:25:27 +0700

    feat: finish migrations

--- db/migrations/20240515232540_add_nurses_table.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE nurses (
-    id SERIAL PRIMARY KEY,
-    nip VARCHAR(13) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    identity_card_scan_img TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
