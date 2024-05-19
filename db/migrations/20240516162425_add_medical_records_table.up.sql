2adc431f7268425e720f594616a793810a0347c1
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-05-19 16:50:58 +0700

    change column name

--- db/migrations/20240516162425_add_medical_records_table.up.sql
+++ db/migrations/20240516162425_add_medical_records_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE medical_records (
-    identityNumber NUMERIC(16) NOT NULL,
+    identity_number NUMERIC(16) NOT NULL,
     symptoms VARCHAR(2000) NOT NULL,
     medications VARCHAR(2000) NOT NULL,
     created_by VARCHAR(255),
